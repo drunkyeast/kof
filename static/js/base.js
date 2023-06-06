25b03663038fb333dfa910ae09e4c2c269f9713f
drunkyeast <jmjzhangqiao@gmail.com>
2023-06-06 17:14:50 +0800

    new file:   static/css/base.css 	new file:   static/images/background/0.gif 	new file:   static/images/player/kyo/0.gif 	new file:   static/images/player/kyo/1.gif 	new file:   static/images/player/kyo/2.gif 	new file:   static/images/player/kyo/3.gif 	new file:   static/images/player/kyo/4.gif 	new file:   static/images/player/kyo/5.gif 	new file:   static/images/player/kyo/6.gif 	new file:   static/js/ac_game_object/base.js 	new file:   static/js/base.js 	new file:   static/js/controller/base.js 	new file:   static/js/game_map/base.js 	new file:   static/js/player/base.js 	new file:   static/js/player/kyo.js 	new file:   static/js/utils/gif.js 	new file:   templates/index.html

---
+++ static/js/base.js
@@ -0,0 +1,34 @@
+import { GameMap } from '/static/js/game_map/base.js'
+import { Kyo } from '/static/js/player/kyo.js'
+
+class KOF {
+    constructor(id) {
+        this.test = "hello from /static/js/base.js"
+        this.$kof = $('#' + id);
+        this.game_map = new GameMap(this);
+
+        this.players = [
+            new Kyo(this, {
+                id: 0,
+                x: 200,
+                y: 0,
+                width: 120,
+                height: 220,
+                color: 'blue'
+            }),
+            new Kyo(this, {
+                id: 1,
+                x: 900,
+                y: 0,
+                width: 120,
+                height: 220,
+                color: 'red'
+            })
+        ];
+
+    }
+}
+
+export {
+    KOF
+}
